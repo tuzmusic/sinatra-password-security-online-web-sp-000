1fa1aa74d5c6d3dd69e88fe1981850ccacb0f941
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-20 16:20:11 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,11 @@ require 'capybara/dsl'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+ActiveRecord::Base.logger = nil
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
 require_all 'app'
-
